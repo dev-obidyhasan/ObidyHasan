f7ae4f9020c9ddf5ffee28cdef0f47747d4ef7bf
obidyhasan <obidyhasan@gmail.com>
2025-12-26 10:52:21 +0600

    include resume in navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,31 +1,27 @@
-"use client";
 import Link from "next/link";
 import { Jersey_10 } from "next/font/google";
 import { Button } from "../ui/button";
 import { MagicCard } from "../ui/magic-card";
 import NavbarMenu from "../modules/Public/Navbar/NavbarMenu";
 import NavbarPopover from "../modules/Public/Navbar/NavbarPopover";
-import Cookies from "js-cookie";
+import { IProfile } from "@/types";
 
 const jersey = Jersey_10({
   weight: "400",
   subsets: ["latin"],
 });
 
-const Navbar = () => {
-  function isLoggedIn() {
-    const token = Cookies.get("accessToken");
-    const email = Cookies.get("email");
-
-    if (!token || !email) {
-      return false;
+const Navbar = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/user?email=${process.env.NEXT_PUBLIC_ADMIN_EMAIL}`,
+    {
+      next: {
+        revalidate: 30,
+      },
     }
-
-    if (email !== process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
-      return false;
-    }
-    return true;
-  }
+  );
+  const { data } = await res.json();
+  const profile: IProfile = data;
 
   return (
     <div className="max-w-6xl mx-auto sticky top-4 px-4  z-[100]">
@@ -44,13 +40,15 @@ const Navbar = () => {
           </div>
           <NavbarMenu />
           <div className="w-fit">
-            {isLoggedIn() ? (
-              <Button className="rounded-full" variant={"secondary"} asChild>
-                <Link href={"/dashboard/profile"}>Dashboard</Link>
+            {profile?.resumeUrl ? (
+              <Button className="rounded-full" variant={"secondary"}>
+                <Link href={profile?.resumeUrl} target="_blank">
+                  Resume
+                </Link>
               </Button>
             ) : (
               <Button className="rounded-full" variant={"secondary"} asChild>
-                <Link href={"/login"}>Login</Link>
+                <Link href={"/"}>Resume</Link>
               </Button>
             )}
           </div>
